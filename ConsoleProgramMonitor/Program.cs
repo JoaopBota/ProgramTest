4312b1345cbb518bfed625e959c14b6a541178dc
JoaoBota <soaresbota@gmail.com>
2024-04-25 19:07:01 +0100

    Inittial Push and test
    Signed-off-by: JoaoBota <soaresbota@gmail.com>

---
+++ ConsoleProgramMonitor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GetProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] list = Process.GetProcessesByName("test");
+        
+            foreach (var process in list)
+            {
+                Console.Out.WriteLine(process.ProcessName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
