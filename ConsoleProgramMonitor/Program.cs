585367869c35ff687b27660bb10c5e8a7ad38d9c
JoaoBota <soaresbota@gmail.com>
2024-04-27 17:15:34 +0100

    Updated Console text, and added the feature to kill the program
    Signed-off-by: JoaoBota <soaresbota@gmail.com>

--- ConsoleProgramMonitor/Program.cs
+++ ConsoleProgramMonitor/Program.cs
@@ -9,27 +9,43 @@ namespace GetProcess
         {
             if (args.Length == 3)
             {
-                string ProcessName = args[0];
+                string processName = args[0];
+                int maxLifetime = int.Parse(args[1]);
+                int monitoringFrequency = int.Parse(args[2]);
 
-                Process[] processes = Process.GetProcessesByName(ProcessName);
+                Process[] processes = Process.GetProcessesByName(processName);
 
-                if(processes.Length > 0){
-                    Console.WriteLine("Process " + ProcessName + " is running.");
+                if (processes.Length > 0)
+                {
+                    Console.WriteLine($"Info: Process '{processName}' is running.");
 
-                }
-
-                else{
-                    Console.WriteLine("Error: The process " + ProcessName +" is not running!");
+                    Console.WriteLine($"Info: Program.exe  is currently monitoring '{processName}' with a max lifetime of : [ '{processName}' minute(s) ] and with a monitoring frequency of : [ '{processName}' minute(s) ]");
+                    while (true)
+                    {
+                        foreach (var process in processes)
+                        {
+    
+                                TimeSpan processLifetime = DateTime.Now - process.StartTime;
+                                if (processLifetime.TotalMinutes > maxLifetime)
+                                {
+                                    Console.WriteLine($"Process '{processName}' has exceeded the maximum lifetime. Killing process...");
+                                    process.Kill();
+                                }
+                            }
+                            Thread.Sleep(monitoringFrequency * 60 * 1000); // Convert minutes to milliseconds
+                        }
+                    }
+                else
+                {
+                    Console.WriteLine($"Error: The process '{processName}' is not running!");
                     return;
                 }
-            
             }
             else if (args.Length != 3)
             {
                 Console.WriteLine("Error: Three arguments are required!");
                 return;
             }
-
         }
     }
 }
